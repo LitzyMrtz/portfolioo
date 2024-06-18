87a6dac5fe56345499750134a8830b7cc18c761b
LitzyMrtz <litzymrtz.160512@gmail.com>
2024-06-18 14:43:34 -0400

    Add files via upload

---
+++ game.py
@@ -0,0 +1,16 @@
+import pygame
+import math
+
+pygame.init()
+
+running= True
+
+width= 1000
+height= 800
+
+screen_size = (width,height)
+
+pygame.display.set_caption('Two Players and collison')
+
+
+#<html><head><title>Let's play!!</title></head><body><h2> </h2></body></html>
